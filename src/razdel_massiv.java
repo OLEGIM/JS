5d2e4f90236025a9357df03e9edfa27b329efdff
olegim <OLEGIM.89@mail.ru>
2017-06-23 13:59:31 +0300

    less1+

---
+++ src/razdel_massiv.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by OLEG on 23.06.2017.
+ */
+public class razdel_massiv {
+    public static void main (String [] args){
+        int [] array = new int[12]; // иницилизируем массив, размером 12
+        Random random = new Random();
+
+        for (int i=0; i < array.length; i++){
+            array[i] = random.nextInt(60); //диапопазон значения
+        }
+        System.out.println(Arrays.toString(array));
+
+        int[] arr1 = Arrays.copyOfRange(array,0,array.length/2); //С начала и до половины разбиваем
+        int[] arr2 = Arrays.copyOfRange(array,array.length/2,array.length); //Разбиваем  с половины до конца
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+
+    }
+}
--- src/union_massiv.java
+++ src/union_massiv.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+
 /**
  * Created by OLEG on 23.06.2017.
  */
 public class union_massiv {
     public static void main (String[] args){
-    
+        int [] arr1 = {1,-5,95,0,23,42};
+        int [] arr2 = {72, 621, -7,12};
+        int [] array = new int [arr1.length + arr2.length];
+
+        System.arraycopy(arr1,0,array,0,arr1.length);
+        System.arraycopy(arr2,0,array, arr1.length,arr2.length);
+        System.out.println(Arrays.toString(array));
     }
 }
