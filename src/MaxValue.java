eb35ef56fb4d3b0c4571e90fc6f7c418873da2c7
olegim <OLEGIM.89@mail.ru>
2017-06-27 18:21:56 +0300

    MaxValue

---
+++ src/MaxValue.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+/**
+ * Created by OLEG on 27.06.2017.
+ */
+
+public class MaxValue {
+
+
+        public static void main (String[] args){
+            int[] array = new int[20];
+            int Max;
+            Random random = new Random();
+
+            for (int i= 0; i < array.length; i++){
+                array[i] = random.nextInt(10000);
+            }
+
+            Max = array[0];
+            for(int i=0; i < array.length; i++){
+                if (Max>array[i]){
+                    Max=array[i];
+                }
+            }
+            System.out.println("Max = " + Max);
+        }
+    }
+
+
+
+
