ac4caae122947e70af7b4f1b93cca64ac8c846ab
olegim <OLEGIM.89@mail.ru>
2017-06-28 16:12:07 +0300

    First TOM

--- src/AddElement.java
+++ src/AddElement.java
@@ -1,7 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by OLEG on 27.06.2017.
  */
 public class AddElement {
-}
+    public static void main (String[] args){
+        int [] array = new int[10];
+        Random random = new Random();
+        int length = array.length;
+        int value = -5;
+
+        for (int i=0; i < array.length; i++){
+        array[i] =random.nextInt(50);
+    }
+        System.out.println(Arrays.toString(array));
+        System.out.println("length of massiv " + length);
+
+        array = addElement(array, value);
+        length = array.length;
+        System.out.println(Arrays.toString(array));
+        System.out.println("length of masssiv " + length);
+    }
+
+    public static int[] addElement(int[] array, int value) {
+        int [] newArray = new int[array.length+1]; //увкличиваем массив на 1
+        for (int i =0; i < newArray.length; i++){
+            if (i < array.length)
+                newArray[i] = array[i]; //добавим значение в newArray
+                        else
+                            newArray[i]= value;
+        }
+        return newArray;
+    }
+    }
--- src/DelElement.java
+++ src/DelElement.java
@@ -1,7 +1,57 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by raja on 28.06.17.
  */
 public class DelElement {
+
+
+    public static void main (String [] args){
+        int [] array = new int [10];
+        Random random= new Random();
+        int length = array.length;
+        int value = 4;
+
+        for(int i =0; i < array.length; i++){
+            array[i]= random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Dlina massiva = " + length);
+        System.out.println(Arrays.asList(array).contains("4"));
+
+        array=removeByValue(array, value);
+        length=array.length;
+        System.out.println(Arrays.toString(array));
+        System.out.println("Dlina massiva = " + length);
+
+
+    }
+
+    public static int[] removeByValue(int[] array, int value) {
+        int valueIndex = -1;
+
+        for(int i=0; i < array.length; i++){
+            if (array[i] == value){
+                valueIndex = i;
+                break;
+            }
+        }
+        if (valueIndex < 0){
+            return array;
+        }
+        else{
+            int[] newArray = new int[array.length - 1];
+            for(int i =0; i < valueIndex; i++){
+                newArray[i] = array[i];
+            }
+            for(int i = valueIndex + 1; i < array.length; i++){
+                newArray[i-1]=array[i];
+            }
+            array = newArray;
+
+                return array;
+        }
+
+    }
 }
