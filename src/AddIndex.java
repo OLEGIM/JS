7acd4417b8dde33ce4de51868e3fd3be326343cb
olegim <OLEGIM.89@mail.ru>
2017-06-28 16:12:07 +0300

    First TOM

--- src/AddIndex.java
+++ src/AddIndex.java
@@ -1,7 +1,48 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by raja on 28.06.17.
  */
 public class AddIndex {
+    public static void main (String [] args){
+        int [] array = new int[10];
+        Random random = new Random();
+        int length = array.length;
+        int value = -5;
+        int index = 4;
+
+        for(int i=0; i < array.length; i++){
+            array[i]= random.nextInt(50);
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Dlina massiva + " + length);
+
+        array=addElementByIndex(array, value, index);
+        length = array.length;
+
+        System.out.println(Arrays.toString(array));
+        System.out.println("Dlina massiva + " + length);
+
+
+    }
+
+public static int[] addElementByIndex(int[] array, int value, int index) {
+        if (index < 0|| index  > array.length){
+            System.out.println("Wrong index");
+            return array;
+        }
+        int [] newArray = new int [array.length+1];
+        for(int i = 0; i< newArray.length;i++){
+            if(i< index)
+                newArray[i] = array[i];
+            else if (i == index)
+                newArray[i] = value;
+            else
+                newArray[i] = array[i-1];
+        }
+    return newArray;
+    }
+
 }
+
---
+++ src/DelIndex.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by OLEG on 28.06.2017.
+ */
+public class DelIndex {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        int length = array.length;
+        int index = 4;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50);
+        }
+        System.out.println(Arrays.toString(array)); //output array elements
+        System.out.println("Dlina massiva =  " + length);
+
+        array = removeByIndex(array, index);
+        length = array.length;
+        System.out.println(Arrays.toString(array));
+        System.out.println("Dlina massiva =  " + length);
+
+    }
+
+    public static int[] removeByIndex(int[] array, int index) {
+        if (index < 0 || index > array.length) {
+            System.out.println("Wrong index");
+            return array;
+        }
+        int[] newArray = new int[array.length - 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < index)
+                newArray[i] = array[i];
+            else if (i >= index)
+                newArray[i] = array[i + 1];
+
+        }
+        return newArray;
+    }
+}
+
+
+
